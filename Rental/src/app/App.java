3de893e88f6c047b3ab52e9175f88186136431a4
Naveen William <66348947+GhostInHex-x86@users.noreply.github.com>
2026-05-31 18:20:18 +0530

    Refactor vehicle storage from ArrayList to HashMap

--- Rental/src/app/App.java
+++ Rental/src/app/App.java
@@ -4,7 +4,7 @@ import models.*;
 import manager.VehicleManager;
 
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Random;
@@ -13,20 +13,24 @@ public class App {
     static Scanner scanner = new Scanner(System.in);
     static Random gen = new Random();
 
-    // polymorphic Array
-    static ArrayList<Vehicle> vehicles = new ArrayList<>();
+    // polymorphic HashMap
+    static HashMap<Integer, Vehicle> vehicles = new HashMap<>();
+    static Vehicle vTemp;
 
     public static void main(String[] args) {
-        // Array instantiation
-        vehicles.add(new Car("Ford Fiesta", 1500.90, Math.abs(gen.nextInt(100, 1000)), 4, true));
-        vehicles.add(new Car("Nexon EV", 900.15, Math.abs(gen.nextInt(100, 1000)), 4, true));
-        vehicles.add(new Car("Bolero", 2500.70, Math.abs(gen.nextInt(100, 1000)), 5, false));
-        vehicles.add(new Bike("Bullet 500", 2000, Math.abs(gen.nextInt(100, 1000)), 5, true));
-        vehicles.add(new Bike("Honda CB350", 1500, Math.abs(gen.nextInt(100, 1000)), 6, false));
-        vehicles.add(new Bike("Himalayan 411", 35000, Math.abs(gen.nextInt(100, 1000)), 6, true));
-        vehicles.add(new Truck("Ashok Leyland", 5000, Math.abs(gen.nextInt(100, 1000)), 1500, true));
+        // Dummy Vehicles instantiation
+        addDummyVehicle(new Car("Ford Fiesta", 1500.90, Math.abs(gen.nextInt(100, 1000)), 4, true));
+        addDummyVehicle(new Car("Nexon EV", 900.15, Math.abs(gen.nextInt(100, 1000)), 4, true));
+        addDummyVehicle(new Car("Bolero", 2500.70, Math.abs(gen.nextInt(100, 1000)), 5, false));
+        addDummyVehicle(new Bike("Bullet 500", 2000, Math.abs(gen.nextInt(100, 1000)), 5, true));
+        addDummyVehicle(new Bike("Honda CB350", 1500, Math.abs(gen.nextInt(100, 1000)), 6, false));
+        addDummyVehicle(new Bike("Himalayan 411", 35000, Math.abs(gen.nextInt(100, 1000)), 6, true));
+        addDummyVehicle(new Truck("Ashok Leyland", 5000, Math.abs(gen.nextInt(100, 1000)), 1500, true));
 
+        // Initializing manager service
         VehicleManager manager = new VehicleManager(vehicles, scanner);
+
+        // Main loop
         boolean isRunning = true;
         int choice;
         try {
@@ -49,7 +53,7 @@ public class App {
                         case 4 -> manager.returnVeh();
                         case 5 -> manager.changeRent();
                         case 6 -> isRunning = false;
-                        case 00 -> magicDebug();
+                        case 0 -> magicDebug();
                         default -> {
                             System.out.println("Invalid Choice!");
                             continue;
@@ -74,12 +78,17 @@ public class App {
         }
     }
 
+    private static void addDummyVehicle(Vehicle vehicle) {
+        vehicles.put(vehicle.getID(), vehicle);
+    }
+
     static void showVeh() {
         System.out.println("\n-------------------");
         System.out.println("Vehicles");
         System.out.println("-------------------");
+        
         // Runtime Polymorphism
-        for (Vehicle vehicle : vehicles) {
+        for (Vehicle vehicle : vehicles.values()) {
             vehicle.displayInfo();
         }
         System.out.println("\n[+]Available for Renting: " + Vehicle.availableVehCount);
@@ -119,33 +128,37 @@ public class App {
         System.out.print("Vehicle ID: ");
         int id = scanner.nextInt();
         scanner.nextLine();
+        if (vehicles.containsKey(id)) {
+            System.out.println("Vehicle Already exists with this ID!");
+            return;
+        }
         switch (type) {
             case BIKE -> {
                 System.out.print("Enter gears: ");
                 gears = scanner.nextInt();
                 scanner.nextLine();
-                vehicles.add(new Bike(name, rent, id, gears));
+                vehicles.put(id, new Bike(name, rent, id, gears));
                 System.out.println("Bike added successfully.");
             }
             case CAR -> {
                 System.out.print("Enter seats: ");
                 seats = scanner.nextInt();
                 scanner.nextLine();
-                vehicles.add(new Car(name, rent, id, seats));
+                vehicles.put(id, new Car(name, rent, id, seats));
                 System.out.println("Car added successfully.");
             }
             case TRUCK -> {
                 System.out.print("Enter max load:");
                 load = scanner.nextInt();
                 scanner.nextLine();
-                vehicles.add(new Truck(name, rent, id, load));
+                vehicles.put(id, new Truck(name, rent, id, load));
                 System.out.println("Truck added successfully.");
             }
         }
     }
 
     static void magicDebug() {
-        for (Vehicle vehicle : vehicles) {
+        for (Vehicle vehicle : vehicles.values()) {
             System.out.println(vehicle.toString());
         }
     }
